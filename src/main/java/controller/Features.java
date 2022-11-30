f120102dc309cf5f2bc9e66482a34f31b6144733
rashijain <rashijain2637@gmail.com>
2022-11-30 16:57:28 -0500

    added comments

--- src/main/java/controller/Features.java
+++ src/main/java/controller/Features.java
@@ -4,25 +4,81 @@ import javax.swing.*;
 
 public interface Features {
 
+  /**
+   * This function creates a new flexible portfolio.
+   *
+   * @param frame      the JPanel that the user is currently on
+   * @param text       The name of the portfolio
+   * @param dayText    The day of the month that the user wants to start the portfolio on.
+   * @param monthText  The month of the date you want to start the portfolio on.
+   * @param yearText   The year of the portfolio
+   * @param tickerText The ticker symbol of the stock you want to add to the portfolio.
+   * @param numberText the number of shares you want to buy
+   */
   void createNewFlexiblePortfolio(JPanel frame, String text, String dayText, String monthText,
                                   String yearText,
                                   String tickerText, String numberText);
 
+  /**
+   * This function sells portfolio.
+   *
+   * @param frame      the JPanel that the user is currently on
+   * @param text       the name of the portfolio
+   * @param dayText    The day of the month you want to sell the stock
+   * @param monthText  The month of the date you want to sell the stock.
+   * @param yearText   the year of the date you want to sell the stock
+   * @param tickerText the ticker symbol of the stock you want to sell
+   * @param numberText The number of shares you want to sell
+   */
   void sellPortfolio(JPanel frame, String text, String dayText, String monthText,
                      String yearText,
                      String tickerText, String numberText);
 
+  /**
+   * This function calculates cost Basis.
+   *
+   * @param frame     The JPanel that the cost basis will be displayed on.
+   * @param name      The name of the stock
+   * @param dayText   The day of the month the stock was purchased.
+   * @param monthText The month of the purchase.
+   * @param yearText  The year of the purchase.
+   */
   void costBasis(JPanel frame, String name, String dayText,
                  String monthText,
                  String yearText);
 
+  /**
+   * This function total value of stocks.
+   *
+   * @param frame     The JPanel that the user is currently on.
+   * @param name      name of portfolio.
+   * @param dayText   The day of the month.
+   * @param monthText The month of the date you want to calculate the total value of.
+   * @param yearText  The year of the date you want to calculate the total value of.
+   */
   void totalValue(JPanel frame, String name, String dayText, String monthText,
                   String yearText);
 
+  /**
+   * It uploads a portfolio to the current session.
+   *
+   * @param frame    The JPanel that the user is currently on.
+   * @param path     The path to the file you want to upload.
+   * @param selected 1 for inflexible, 2 for flexible
+   */
   void uploadPortfolio(JPanel frame, String path, int selected);
 
+  /**
+   * It calculates the dollar cost averaging of a stock.
+   */
   void dollarCostAveraging();
 
+  /**
+   * This function takes in a ticker and a percentage.
+   *
+   * @param ticker     The ticker symbol of the stock you want to get the data for.
+   * @param percentage The percentage of the stock's price that you want to buy.
+   */
   void noDate(String ticker, String percentage);
 
   void dollarCostAveragingAndQueryCostBasisAndValue();
--- src/main/java/view/ViewGui.java
+++ src/main/java/view/ViewGui.java
@@ -4,15 +4,52 @@ import javax.swing.*;
 
 import controller.ControllerGUIImpl;
 
+// Creating a new interface called ViewGui.
 public interface ViewGui {
 
+  /**
+   * * This function adds the features to the controller.
+   *
+   * @param features The features to add to the controller.
+   */
+  /**
+   * * This function adds the features to the controller.
+   *
+   * @param features The features to add to the controller.
+   */
   void addFeatures(ControllerGUIImpl features);
 
+  /**
+   * It displays the total value of the company's stock.
+   *
+   * @param frame       The JPanel that you want to display the data on.
+   * @param companyName The name of the company you want to display the data for.
+   * @param value       The value to be displayed
+   */
   void displayDynamicDataTotalValue(JPanel frame, String companyName, String value);
 
+  /**
+   * This function displays the total value of a portfolio on the screen.
+   *
+   * @param frame         The JPanel that the total value will be displayed on.
+   * @param portfolioName The name of the portfolio
+   * @param value         The total value of the portfolio
+   * @param date          The date of the total value.
+   */
   void displayTotalValue(JPanel frame, String portfolioName, String value, String date);
 
+  /**
+   * Create a message box with the given message and add it to the given frame.
+   *
+   * @param frame   The frame that the message box will be created on.
+   * @param message The message you want to display in the message box.
+   */
   void createMessageBox(JPanel frame, String message);
 
+  /**
+   * This function takes in a string of data and displays the cost basis of the stock.
+   *
+   * @param data The data to be displayed.
+   */
   void displayCostBasis(String data);
 }
